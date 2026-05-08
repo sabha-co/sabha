28bcd7f0d8e193b2fb0e9db8b0f8a950c4e185ee
Ashwin M <thatsashwin@gmail.com>
2026-05-08 05:41:56 +0530

    Stop admins from banning, unbanning, or deactivating themselves (#61)

--- app/controllers/users/bans_controller.rb
+++ app/controllers/users/bans_controller.rb
@@ -3,11 +3,13 @@ class Users::BansController < ApplicationController
   before_action :set_user
 
   def create
+    return redirect_to @user, alert: ban_rejection_alert unless @user.bannable_by?(Current.user)
     @user.ban
     redirect_to @user
   end
 
   def destroy
+    return redirect_to @user, alert: unban_rejection_alert unless @user.unbannable_by?(Current.user)
     @user.unban
     redirect_to @user
   end
@@ -15,5 +17,21 @@ class Users::BansController < ApplicationController
   private
     def set_user
       @user = User.find(params[:user_id])
+    end
+
+    def ban_rejection_alert
+      if @user == Current.user
+        "You can't ban yourself."
+      elsif @user.banned?
+        "#{@user.name} is already banned."
+      end
+    end
+
+    def unban_rejection_alert
+      if @user == Current.user
+        "You can't unban yourself."
+      elsif !@user.banned?
+        "#{@user.name} is not banned."
+      end
     end
 end
