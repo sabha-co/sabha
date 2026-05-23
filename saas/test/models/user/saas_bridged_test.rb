f65e5c4e9a91d9c7df9805798471302582ac5179
Ashwin M <thatsashwin@gmail.com>
2026-05-23 21:59:26 +0530

    Pin down cross-tenant scoping in User#close_remote_connections

--- saas/test/models/user/saas_bridged_test.rb
+++ saas/test/models/user/saas_bridged_test.rb
@@ -60,4 +60,28 @@ class User::SaasBridgedTest < ActiveSupport::TestCase
   ensure
     identity&.destroy
   end
+
+  test "close_remote_connections scopes the disconnect by current_tenant in SaaS mode" do
+    identity = GlobalIdentity.create!(name: "RC", email_address: "remote-conn@example.com", verified_at: Time.current)
+
+    with_provisioned_workspace(name: "Remote Conn WS", creator: identity) do |workspace|
+      tenant_id = workspace.external_id.to_s
+      membership = WorkspaceMembership.find_by(tenant: tenant_id)
+
+      ApplicationRecord.with_tenant(tenant_id) do
+        user = User.find(membership.user_id)
+
+        connections = mock("RemoteConnections")
+        connections.expects(:disconnect).with(reconnect: false)
+        ActionCable.server.remote_connections
+          .expects(:where)
+          .with(current_tenant: tenant_id, current_user: user)
+          .returns(connections)
+
+        user.send(:close_remote_connections)
+      end
+    end
+  ensure
+    identity&.destroy
+  end
 end
